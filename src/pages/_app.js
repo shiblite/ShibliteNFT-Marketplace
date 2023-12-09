b1fcd0100323f498f603e86c6dd5b1449433abcf
xdefidev <xdefidev@gmail.com>
2023-12-09 22:43:59 +0100

    ch

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -915,7 +915,7 @@ export default function App({ Component, pageProps }) {
             tokenURI,
             db.collection("User").record(signer_address),
             db.collection("Collection").record(_tokenURI.collection),
-            _tokenURI.properties[0]
+            _tokenURI?.properties[0]?.type
               ? JSON.stringify(_tokenURI.properties)
               : "[]",
             _tokenURI.name,
